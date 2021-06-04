f83e694bbbfd6ef24d87f4f040ce61c7fb3495ed
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-06-03 20:54:51 -0500

    Initial Commit

---
+++ apps/socket-server-01/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'socket-server-01',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/socket-server-01',
+};
